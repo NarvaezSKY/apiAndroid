acb067049a5212cdc87d4d23cd3c5343fadecb9a
cristian <127469110+NarvaezSKY@users.noreply.github.com>
2023-12-12 00:05:16 -0500

    post done

--- controllers/motos.controller.js
+++ controllers/motos.controller.js
@@ -2,13 +2,28 @@ import Motos from '../models/motos.model.js'
 
 export const getAllMotos=async(req,res)=>{
     try {
-        const foundMotos=Motos.find()
-        res.send(foundMotos)
+        const foundMotos=await Motos.find()
+        res.status(200).json({foundMotos})
     } catch (error) {
         res.send(error)
     }
 }
 
-export const postMotoso=async(req, res)=>{
+export const postMotos=async(req, res)=>{
+    try {
+        const {placaMoto, marcaMoto, modeloMoto}= req.body
+
+        const newMoto= new Motos({
+            placaMoto,
+            marcaMoto,
+            modeloMoto
+        })
+        const savedMoto=await newMoto.save()
+    
+        res.status(200).json({message:"moto guardada cabron", savedMoto})
+    } catch (error) {
+        res.status(400).json({message:`SERVER ERROR! ${error}`})
+    }
     
 }
+
--- routes/motos.routes.js
+++ routes/motos.routes.js
@@ -1,10 +1,11 @@
-import { getAllMotos } from "../controllers/motos.controller.js";
+import { getAllMotos, postMotos,  } from "../controllers/motos.controller.js";
 import { Router } from "express";
 
 const router=Router()
 
 
-router.get('/getmotos', getAllMotos)
+router.get('/motos', getAllMotos)
+router.post('/motos', postMotos)
 
 
 export default router
